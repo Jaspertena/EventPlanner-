1830f39609426a0004499d128b47193d900a17f7
Fisher Davis <fisherd02@yahoo.com>
2022-05-03 12:03:36 -0400

    created user and comments models

--- models/Comment.js
+++ models/Comment.js
@@ -1,1 +1,43 @@
-// define comment table
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection');
+
+class Comment extends Model {}
+
+Comment.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    commenter_ID: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references:{
+        model: 'user',
+        key: 'id',
+      }
+    },
+    content: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    date_created: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW,
+      },
+   
+  },
+  {
+    sequelize,
+    timestamps: true,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'comments',
+  }
+);
+
+module.exports = Comment;
