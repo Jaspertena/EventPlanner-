86adff1ae8a1e30459f957521def4a45a980195a
Fisher Davis <fisherd02@yahoo.com>
2022-05-04 11:37:28 -0400

    created the controllerAPI index and the controller index files

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,1 +1,12 @@
 // import other api routes and export them as one
+const router = require('express').Router();
+
+const userRoutes = require('./userRoutes');
+const eventRoutes = require('./eventRoutes');
+const commentRoutes = require('./commentRoutes');
+
+router.use('/user', userRoutes);
+router.use('/event', eventRoutes);
+router.use('/comment', commentRoutes);
+
+module.exports = router;
