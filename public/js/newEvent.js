de6962860261bda5823ca18faeb4c6afb80c5ba2
Maddox Twin1 <jaspertena@aol.com>
2022-05-05 12:28:48 -0400

    add new event.handlebars

--- public/js/newEvent.js
+++ public/js/newEvent.js
@@ -1,21 +1,33 @@
-const newFormHandler = async function(event) {
-    event.preventDefault();
-  
-    const title = document.querySelector('input[name="event-title"]').value;
-    const body = document.querySelector('textarea[name="event-body"]').value;
-  
-    await fetch(`/api/post`, {
-      method: 'POST',
-      body: JSON.stringify({
-        title,
-        body,
-      }),
-      headers: { 'Content-Type': 'application/json' },
-    });
-  
-    document.location.replace('/dashboard');
-  };
-  
-  document
-    .querySelector('#new-post-form')
-    .addEventListener('submit', newFormHandler);
+const newEventFormHandler = async function(event) {
+  event.preventDefault();
+
+
+  const nameEl = document.querySelector('#name-input-newEvent');
+
+  const descriptionEl = document.querySelector('#description-input-newEvent');
+
+  const response = await fetch('/api/newEvent', {
+    method: 'POST',
+    body: JSON.stringify({
+      name: nameEl.value,
+      description: descriptionEl.value,
+      event_start: event_startEl.value,
+      event_end: event_endEl.value,
+      location: locationEl.value,
+      cost: costEl.value,
+      link: linkEl.value,
+
+    }),
+    headers: { 'Content-Type': 'application/json' },
+  });
+
+  if (response.ok) {
+    document.location.replace('/homepage');
+  } else {
+    alert('Failed to add event');
+  }
+};
+
+document
+  .querySelector('#newEvent-form')
+  .addEventListener('submit', newEventFormHandler);
