cc7587ad0fc228bb4290346f0da7d47bed4bb6ea
Fisher Davis <fisherd02@yahoo.com>
2022-05-05 13:29:54 -0400

    worked on signup.js file and edited the handlbars files accordingly

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,33 +1,34 @@
-const signupFormHandler = async function(event) {
-    event.preventDefault();
-  
+console.log("whats up");
+const signupFormHandler = async function (event) {
+  event.preventDefault();
+  console.log("hi");
 
-    const emailEl = document.querySelector('#email-input-signup');
+  const nameEl = document.querySelector("#name-input-signup");
+  console.log(nameEl.value);
+  const emailEl = document.querySelector("#email-input-signup");
+  const passwordEl = document.querySelector("#password-input-signup");
+  const addressEl = document.querySelector("#address-input-signup");
+  const phoneNumberEl = document.querySelector("#phoneNumber-input-signup");
+  const birthdayEl = document.querySelector("#birthday-input-signup");
 
-    const passwordEl = document.querySelector('#password-input-signup');
-  
-    const response = await fetch('/api/user', {
-      method: 'POST',
-      body: JSON.stringify({
-        username: emailEl.value,
-        password: passwordEl.value,
-        name: nameEl.value,
-        address: addressEl.value,
-        phoneNumber: phoneNumberEl.value,
-        birthday: birthdayEl.value,
+  const response = await fetch("/api/user", {
+    method: "POST",
+    body: JSON.stringify({
+      name: nameEl.value,
+      email: emailEl.value,
+      password: passwordEl.value,
+    }),
+    headers: { "Content-Type": "application/json" },
+  });
 
-      }),
-      headers: { 'Content-Type': 'application/json' },
-    });
-  
-    if (response.ok) {
-      document.location.replace('/homepage');
-    } else {
-      alert('Failed to sign up');
-    }
-  };
-  
-  document
-    .querySelector('#signup-form')
-    .addEventListener('submit', signupFormHandler);
-  
+  if (response.ok) {
+    document.location.replace("/signup");
+  } else {
+    alert("Failed to sign up");
+  }
+};
+
+document
+  .querySelector("#signup-form")
+  .addEventListener("submit", signupFormHandler);
+console.log(document.querySelector("#signup-form"));
