18f9e1c9cd4608ae34bd721d885048bb0e274dc1
Maddox Twin1 <jaspertena@aol.com>
2022-05-04 20:42:01 -0400

    add login.handlebar updated login.js

--- public/js/login.js
+++ public/js/login.js
@@ -1,1 +1,27 @@
 // login page
+const loginFormHandler = async function(event) {
+    event.preventDefault();
+  
+    const emailEl = document.querySelector('#email-input-login');
+    const passwordEl = document.querySelector('#password-input-login');
+  
+    const response = await fetch('/api/user/login', {
+      method: 'POST',
+      body: JSON.stringify({
+        username: emailEl.value,
+        password: passwordEl.value,
+      }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+  
+    if (response.ok) {
+      document.location.replace('/dashboard');
+    } else {
+      alert('Failed to login');
+    }
+  };
+  
+  document
+    .querySelector('#login-form')
+    .addEventListener('submit', loginFormHandler);
+  
